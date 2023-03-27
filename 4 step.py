740731760f519c106bff6afb3e22a9b07f309599
MaximTulenev <98822890+MaximTulenev@users.noreply.github.com>
2023-03-27 17:58:48 +0300

    Add files via upload

--- 4 step.py
+++ 4 step.py
@@ -4,28 +4,33 @@ pygame.init()
 
 mainClock = pygame.time.Clock()
 
-WINDOWWIDTH = 400
-WINDOWHEIGHT = 400
+WINDOWWIDTH = 1920
+WINDOWHEIGHT = 1020
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_icon(pygame.image.load('img/player.png'))
 pygame.display.set_caption('Collision Detection')
 
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
-foodCounter = 0
-NEWFOOD = 40
 FOODSIZE = 20
-player = pygame.Rect(300, 100, 50, 50)
+player = pygame.Rect(300, 100, 80, 80)
+playerImage = pygame.image.load('img/player.png')
+playerStretchedImage = pygame.transform.scale(playerImage, (80, 80))
+foodImage = pygame.image.load('img/apple.png')
 foods = []
-for i in range(20):
+for i in range(100):
     foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+
+foodCounter = 0
+NEWFOOD = 4
 
 moveLeft = False
 moveRight = False
 moveUp = False
 moveDown = False
-MOVESPEED = 6
+MOVESPEED = 10
 
 while True:
     for event in pygame.event.get():
@@ -46,13 +51,25 @@ while True:
                 moveUp = False
                 moveDown = True
 
+        if event.type == KEYUP:
+            if event.key == K_LEFT or event.key == K_a:
+                moveLeft = False
+            if event.key == K_RIGHT or event.key == K_d:
+                moveRight = False
+            if event.key == K_UP or event.key == K_w:
+                moveUp = False
+            if event.key == K_DOWN or event.key == K_s:
+                moveDown = False
+            if event.key == K_x:
+                player.top = random.randint(0, WINDOWHEIGHT - player.height)
+                player.left = random.randint(0, WINDOWWIDTH - player.width)
+
         if event.type == MOUSEBUTTONUP:
-            foods.append(pygame.Rect(event.pos[0], event.pos[1], FOODSIZE, FOODSIZE))
+            foods.append(pygame.Rect(event.pos[0] - 10, event.pos[1] - 10, 20, 20))
     foodCounter += 1
     if foodCounter >= NEWFOOD:
         foodCounter = 0
-        foods.append(
-            pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+        foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - 20), random.randint(0, WINDOWHEIGHT - 20), 20, 20))
     windowSurface.fill(WHITE)
 
     if moveDown and player.bottom < WINDOWHEIGHT:
@@ -63,13 +80,17 @@ while True:
         player.left -= MOVESPEED
     if moveRight and player.right < WINDOWWIDTH:
         player.right += MOVESPEED
+    windowSurface.blit(playerStretchedImage, player)
 
-    pygame.draw.rect(windowSurface, BLACK, player)
+    # pygame.draw.rect(windowSurface, BLACK, player)
 
     for food in foods[:]:
         if player.colliderect(food):
             foods.remove(food)
-    for i in range(len(foods)):
-        pygame.draw.rect(windowSurface, GREEN, foods[i])
+            player = pygame.Rect(player.left, player.top, player.width + 1, player.height + 1)
+            playerStretchedImage = pygame.transform.scale(playerImage, (player.width, player.height))
+
+    for food in foods:
+        windowSurface.blit(foodImage, food)
     pygame.display.update()
     mainClock.tick(40)
